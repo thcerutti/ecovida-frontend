7782b37cdb38b6dd28f4e5b856fd124a57ffcef0
thcerutti <thiagocerutti@gmail.com>
2025-07-02 21:37:15 -0300

    Refactor painel.css and painel.html for improved structure; add painel.js for description toggling functionality

---
+++ painel-ecossistemas/painel.js
@@ -0,0 +1,16 @@
+function esconderTodos() {
+  const descricoes = document.querySelectorAll(".descricao > div");
+  descricoes.forEach((div) => {
+    div.style.display = "none";
+  });
+}
+
+function mostrarDescricao(biomaClass) {
+  esconderTodos();
+  const biomaDescricao = document.querySelector(`.${biomaClass}`);
+  if (biomaDescricao) {
+    biomaDescricao.style.display = "block";
+  }
+}
+
+esconderTodos(); // Esconde todas as descrições inicialmente
